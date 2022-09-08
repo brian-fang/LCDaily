f68dea30e907893aa774d49bd7c21feb8316f0d9
brian-fang <btf5aa@virginia.edu>
2022-09-07 17:34:57 -0700

    use import

--- 94BinaryTreeInorder/IterativeSolution.cc
+++ 94BinaryTreeInorder/IterativeSolution.cc
@@ -1,15 +1,8 @@
+#include "../Templates/TreeNode.hh"
 #include <stack>
 #include <vector>
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(): val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right): val(x), left(left), right(right) {}
-};
 
 // RETURNS the inorder traversal (values) of the tree with root 'node'
 vector<int> inorder(TreeNode* node) {
